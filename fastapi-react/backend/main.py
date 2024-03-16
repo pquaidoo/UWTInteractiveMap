bcbccb4bb2fbde3ecc106254137a2f0a76a4b996
buruk yimesgen <buruky@uw.edu>
2024-03-16 11:04:48 -0700

    PLEASE WORK

--- fastapi-react/backend/main.py
+++ fastapi-react/backend/main.py
@@ -1,4 +1,6 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host = "0.0.0.0", port = 8000, reload = True)
+    print("It is being excuted")
+    uvicorn.run("app.api:app", host = "127.0.0.1", port = 8000, reload = True)
+                
